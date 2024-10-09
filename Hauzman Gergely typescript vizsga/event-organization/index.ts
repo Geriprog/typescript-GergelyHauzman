5e3f66618e54fc3039f1c98eac472b8464295b09
Geriprog <89206762+Geriprog@users.noreply.github.com>
2024-10-09 20:27:55 +0200

    Update index.ts

--- Hauzman Gergely typescript vizsga/event-organization/index.ts
+++ Hauzman Gergely typescript vizsga/event-organization/index.ts
@@ -5,11 +5,10 @@ import { ParticipantList } from "./services/guestslist";
 
 
 
-const event1 = new Event (1, "2025.06.24", "Birthday" );
-const event2 = new Event (2, "2025.08.12", "Concert" );
+const event1 = new Event (1, "2025.06.24", "Birthday", "content" );
+const event2 = new Event (2, "2025.08.12", "Concert", "content" );
 
-const participant1 = new Participant (1, "Ildikó", "woman" );
-const participant2 = new Participant (2, "János", "man" );
-const participant3 = new Participant (3, "Géza", "man" );
-const participant4 = new Participant (4, "Eszter", "woman" );
-
+const participant1 = new Participant (1, "Ildikó", "woman", "content" );
+const participant2 = new Participant (2, "János", "man", "content" );
+const participant3 = new Participant (3, "Géza", "man", "content" );
+const participant4 = new Participant (4, "Eszter", "woman", "content" );
